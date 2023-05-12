3ca2148b443f835e430a223e05a9fce388658067
Abrar Haque <abm.abrar@gmail.com>
2023-05-13 01:10:09 +0200

    topbar done

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import './topbar.css'
+import {NotificationsNone, Language, Settings} from '@mui/icons-material';
 
 const Topbar = () => {
     return (
-        <div>
-            Topbar
+        <div className='topbar'>
+            <div className="topbarWrapper">
+                <div className="topLeft">
+                    <span className="logo">abraradmin</span>
+                </div>
+                <div className="topRight">
+                    <div className="topbarIconContainer">
+                        <NotificationsNone/>
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Language/>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Settings/>
+                    </div>
+                    <img src="https://images.pexels.com/photos/16380570/pexels-photo-16380570.jpeg" alt="" className='topAvatar'/>
+                </div>
+            </div>
         </div>
     );
 }
