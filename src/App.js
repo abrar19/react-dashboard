fb2fb7623c0f093cebcfa8c1b626d374b6d89f24
Abrar Haque <abm.abrar@gmail.com>
2023-05-13 05:31:58 +0200

    Sidebar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
+import './app.css'
 
 function App() {
   return (
     <div className="App">
       <Topbar/>
+      <div className="container">
+        <Sidebar/>
+      </div>
     </div>
   );
 }
