8c8e43c307a74884f2a1876ce92072afc856232d
Abrar Haque <abm.abrar@gmail.com>
2023-05-17 01:45:58 +0200

    User Table Done

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import './app.css'
 import Home from "./pages/home/Home";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
 
 function App() {
   return (
-    <div className="App">
+    <Router className="App">
       <Topbar/>
       <div className="container">
         <Sidebar/>
-        <Home/>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route path="/users" element={<UserList/>} />
+          <Route path="/users/:id" element={<User/>} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/dummyData.js
+++ src/dummyData.js
@@ -28,3 +28,86 @@ export const userData = [
       "Active User": 3490
     },
   ];
+
+  export const userRows = [
+    { 
+        id: 1, 
+        username: 'Cersei Lannister',
+        avatar: 'https://images.pexels.com/photos/2738208/pexels-photo-2738208.jpeg',
+        email: 'cersei@email.com',
+        status: 'active',
+        transaction: '$120.69'
+    },
+    { 
+        id: 2, 
+        username: 'Cersei Lannister',
+        avatar: 'https://images.pexels.com/photos/2738208/pexels-photo-2738208.jpeg',
+        email: 'cersei@email.com',
+        status: 'active',
+        transaction: '$120.69'
+    },
+    { 
+        id: 3, 
+        username: 'Cersei Lannister',
+        avatar: 'https://images.pexels.com/photos/2738208/pexels-photo-2738208.jpeg',
+        email: 'cersei@email.com',
+        status: 'inactive',
+        transaction: '$120.69'
+    },
+    { 
+        id: 4, 
+        username: 'Cersei Lannister',
+        avatar: 'https://images.pexels.com/photos/2738208/pexels-photo-2738208.jpeg',
+        email: 'cersei@email.com',
+        status: 'active',
+        transaction: '$120.69'
+    },
+    { 
+        id: 5, 
+        username: 'Cersei Lannister',
+        avatar: 'https://images.pexels.com/photos/2738208/pexels-photo-2738208.jpeg',
+        email: 'cersei@email.com',
+        status: 'active',
+        transaction: '$120.69'
+    },
+    { 
+        id: 6, 
+        username: 'Cersei Lannister',
+        avatar: 'https://images.pexels.com/photos/2738208/pexels-photo-2738208.jpeg',
+        email: 'cersei@email.com',
+        status: 'inactive',
+        transaction: '$120.69'
+    },
+    { 
+        id: 7, 
+        username: 'Cersei Lannister',
+        avatar: 'https://images.pexels.com/photos/2738208/pexels-photo-2738208.jpeg',
+        email: 'cersei@email.com',
+        status: 'active',
+        transaction: '$120.69'
+    },
+    { 
+        id: 8, 
+        username: 'Cersei Lannister',
+        avatar: 'https://images.pexels.com/photos/2738208/pexels-photo-2738208.jpeg',
+        email: 'cersei@email.com',
+        status: 'inactive',
+        transaction: '$120.69'
+    },
+    { 
+        id: 9, 
+        username: 'Cersei Lannister',
+        avatar: 'https://images.pexels.com/photos/2738208/pexels-photo-2738208.jpeg',
+        email: 'cersei@email.com',
+        status: 'active',
+        transaction: '$120.69'
+    },
+    { 
+        id: 10, 
+        username: 'Cersei Lannister',
+        avatar: 'https://images.pexels.com/photos/2738208/pexels-photo-2738208.jpeg',
+        email: 'cersei@email.com',
+        status: 'active',
+        transaction: '$120.69'
+    }
+];
